e7130589b0673feb0a7e62b1aa561a605bd4f592
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2022-04-08 14:58:58 +0300

    Connect logging

--- random-sequence-generation-service/src/main/java/com/example/random/sequence/generation/service/config/WebSocketHandler.java
+++ random-sequence-generation-service/src/main/java/com/example/random/sequence/generation/service/config/WebSocketHandler.java
@@ -1,6 +1,7 @@
 package com.example.random.sequence.generation.service.config;
 
-import org.springframework.web.socket.BinaryMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
@@ -8,15 +9,11 @@ import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 import java.io.IOException;
 
 public class WebSocketHandler extends AbstractWebSocketHandler {
-    @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-        System.out.println("Posted message");
-        session.sendMessage(message);
-    }
+    private static final Logger log = LoggerFactory.getLogger(WebSocketHandler.class);
 
     @Override
-    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws IOException {
-        System.out.println("New Binary Message Received");
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        log.info("POSTED MESSAGE: " + message);
         session.sendMessage(message);
     }
 }
