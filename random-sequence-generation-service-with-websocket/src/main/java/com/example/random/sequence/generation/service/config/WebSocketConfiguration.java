bd017fc1bd79e8295443a9a55ef18337a6d2119b
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2022-04-13 00:59:12 +0300

    Prepared the skeleton for the transition of the application to WebSocket

---
+++ random-sequence-generation-service-with-websocket/src/main/java/com/example/random/sequence/generation/service/config/WebSocketConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.random.sequence.generation.service.config;
+
+import com.example.random.sequence.generation.service.handler.WebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new WebSocketHandler(), "/socket").setAllowedOrigins("*");
+    }
+}
